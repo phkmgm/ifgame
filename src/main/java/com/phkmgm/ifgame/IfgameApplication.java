87d6ac173eb43e5384520608c4a5167c9b54073c
phkmgm <phkmgm@gmail.com>
2022-12-20 23:39:10 +0900

    Initial commit

---
+++ src/main/java/com/phkmgm/ifgame/IfgameApplication.java
@@ -0,0 +1,13 @@
+package com.phkmgm.ifgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IfgameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phkmgm/ifgame/IfgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phkmgm.ifgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
